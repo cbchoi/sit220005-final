096d75d95b275edea9c309c5d3d594df1b2c58cb
Changbeom Choi <cbchoi@handong.edu>
2017-12-12 03:47:36 +0000

    Problem05

--- Problem05/README.md
+++ Problem05/README.md
@@ -1,15 +1,11 @@
 # Problem05
-Build a simple adder that add given two values. 
-Assume that the there is only one operator, '+'. 
-Also, user may seperate operand and operator by using white space. 
-For example, user may enter 1000 + 2000, and your program should return 3000. 
-Moreover, your program should satisfy the following requirements.
-+ The length of the operand cannot exceed 100.
-+ User may enter negative number. If the user enters the negative number, you may assume that there is no white space between number and the sign.
-+ Assume that the user does not put alphabet or other symbols, except + and -.
-+ You should dynamically allocate memory using new/delete keywords
-+ 
-Refer the following code skeleton
+
+다음은 간단한 객체지향 프로그램을 설계한 것이다. 다음 다이어그램(class.png)과
+출력결과(output.png)를 보고 클래스 간의 상속관계를 파악하여 객체지향 프로그램을 완성하시오. 
+주의사항. 새로운 변수와 함수는 추가할 수 없음.
+
+![class diagram](https://github.com/cbchoi/sit220005-final/Problem05/class.png)
+
 ```
 int main(int argc, char** argv)
 { 
--- Problem05/problem05.cpp
+++ Problem05/problem05.cpp
@@ -2,17 +2,53 @@
 #include <cstring>
 #include <stdlib.h>
 
-int main(int argc, char** argv)
-{ 
-	char buff[1000];
-    std::cout << "Enter the expression. " << std::endl;
-    std::cin.getline(buff, 1000);
+int main(){
 
-    char* operand1 = strtok(buff, " \t\n");
-    strtok(NULL, " \t\n");
-    char* operand2 = strtok(NULL, " \t\n");
-    
-    std::cout << atoi(operand1) + atoi(operand2) << std::endl;
+	Person *list[100];
+	int count = 0;
 
-    return 0;
+	while(true){
+		int type;
+		int age;
+		char name[100];
+
+	    std::cout<<"Enter the type (0:Professor, 1:Student, 2:GraduateStudent, -1:End) "<<std::endl;
+		std::cout<<">> ";
+		std::cin>>type;
+
+		if(type==-1) break;
+		std::cout<<"Age? ";
+		std::cin>>age;
+		std::cout<<"Name? ";
+		std::cin.getline(name, 100);
+
+		if(type==0)
+			list[count] = new Professor(age, name);
+		if(type==1)
+			list[count] = new Student(age, name);
+		if(type==2)
+			list[count] = new GraduateStudent(age, name);
+
+		list[count++]->setMoreInformation();
+		std::cout<<std::endl;
+	}
+
+	while(1){
+		int num;
+
+		std::cout<<"Choose one member (-1 : exit)"<<std::endl;
+
+		for(int i=0; i<count; i++){
+			std::cout<<"["<<i<<"]"<<" : ";
+			list[i]->printTypeAndName();
+		}
+		std::cout<<">>";
+		std::cin>>num;
+
+		if(num==-1) break;
+		list[num]->printInformation();
+		std::cout<<std::endl;
+	}
+	
+	return 0;
 }
